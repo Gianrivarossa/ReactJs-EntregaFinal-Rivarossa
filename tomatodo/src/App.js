fcf386ca930d36efd2a28f27dca0698414b18943
Gianrivarossa <rivarossagianfranco@gmail.com>
2023-03-20 19:40:22 -0300

    ReactJS-PreEntrega1-Rivarossa

---
+++ tomatodo/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import CartWidget from './components/CartWidget';
+function App() {
+  return (
+<>
+<Navbar/>
+
+</>
+  );
+}
+
+export default App;
---
+++ tomatodo/src/components/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CartWidget() {
+  return (
+    <div>
+      <i class="bi bi-cart3"></i>
+    </div>
+  )
+}
+
+export default CartWidget
